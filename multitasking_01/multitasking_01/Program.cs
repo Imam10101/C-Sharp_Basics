1fe70d6b437ed0dee63530515106b9788a66395f
Imam Mehedi <160725166+Imam10101@users.noreply.github.com>
2024-04-25 10:50:25 +0600

    Add files via upload

---
+++ multitasking_01/multitasking_01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multitasking_01
+{
+    internal class Program
+    {
+        static void createtaskusingfactorymethod()
+        {
+            Console.WriteLine("The task is created using Factory Method");
+        }
+
+        static async Task createtaskusingasync()
+        {
+            Console.WriteLine("The task is created using Async");
+            await Task.Delay(3000);
+        }
+
+        static void createtaskusingaction()
+        {
+            Console.WriteLine("The task is created using Action Delegate");
+        }
+
+        static void createtaskusingdelegate()
+        {
+            Console.WriteLine("The task is created using Delegate");
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("===================================");
+
+            Task factoryTask = Task.Factory.StartNew(() => { createtaskusingfactorymethod(); });
+             await createtaskusingasync();  // or createtaskusingasync().Wait();
+
+            Task actionT = new Task(new Action(createtaskusingaction));
+            actionT.Wait(2000);
+            actionT.Start();
+            
+
+            Task del = new Task(delegate { createtaskusingdelegate(); });
+            del.Start();
+            del.Wait(2000);
+
+
+
+
+            Console.WriteLine("===============All tasks completed.============");
+            Console.ReadKey();
+        }
+    }
+}
+    
+
